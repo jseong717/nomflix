846b43db568f5eac18bbb2c251ff3eb40198a7aa
jseong94 <jseong0717@naver.com>
2020-01-06 18:50:14 +0900

    2020-01-06 first com

---
+++ src/Components/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Header = styled.header`
+    color: white;
+    position:fixed;
+    top:0;
+    left:0;
+    width:100%;
+    height: 50px;
+    display: flex;
+    align-items:center;
+    padding: 0px 10px;
+    background-color:rgba(20, 20, 20, 0.8);
+`;
+
+const List = styled.ul`
+    display:flex;
+    &:hover{
+        background-color: blue;
+    }
+`;
+
+const Item = styled.li`
+
+`;
+
+const SLink = styled(Link)`
+
+`;
+
+export default () => (
+    <Header>
+        <List>
+            <Item>
+                <SLink to="/">Movies</SLink>
+            </Item>
+            <Item>
+                <SLink to="/tv">TV</SLink>
+            </Item>
+            <Item>
+                <SLink to="/search">Search</SLink>
+            </Item>
+        </List>
+    </Header>
+);
